d247b19a47cd24f94d8fb61ed4c9d5b43162a4f5
AldegaArt <anyukhin.artem@gmail.com>
2023-07-09 23:28:06 +0300

    On branch main Your branch is up to date with 'wasm-game-of-life/main'.
    Добавил кнопку старта, останова
    
    Changes to be committed:
    	modified:   www/index.html
    	modified:   www/index.js

--- www/index.js
+++ www/index.js
@@ -4,13 +4,41 @@ const pre = document.getElementById("game-of-life-canvas");
 const universe = Universe.new();
 universe.set_random_cells();
 
+let animationId = null;
+
+
+
+const isPaused = () => {
+    return animationId === null;
+}
+
+const playPauseButton = document.getElementById("play-pause");
+
+const play = () => {
+    playPauseButton.textContent = "⏸";
+    renderLoop();
+};
+
+const pause = () => {
+    playPauseButton.textContent = "▶";
+    cancelAnimationFrame(animationId);
+    animationId = null;
+};
+
+playPauseButton.addEventListener("click", event => {
+    if (isPaused()) {
+      play();
+    } else {
+      pause();
+    }
+  });
+  
 const renderLoop = () => {
     // debugger; //специальная программная точка останова в js, закоментил, чтобы не напрягала. В случае чего разкомментить. 
     pre.textContent = universe.render();
     universe.tick();
-    requestAnimationFrame(renderLoop);
-
-    
+    animationId = requestAnimationFrame(renderLoop); 
 };
 
-requestAnimationFrame(renderLoop);
+play();
+
